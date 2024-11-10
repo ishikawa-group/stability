0d041a3aed49768d04b2fd870678193429232ee4
Long-Brian-Yang <longyang2000110@gmail.com>
2024-11-10 23:17:10 +0900

    updated

--- stability/convex_hull/phase_diagram.py
+++ stability/convex_hull/phase_diagram.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Headers and Imports
-from pymatgen.core import Composition, periodic_table
+from pymatgen.core import Composition
 from pymatgen.entries.computed_entries import ComputedEntry
 from pymatgen.analysis.phase_diagram import GrandPotentialPhaseDiagram
 from pymatgen.entries.compatibility import MaterialsProject2020Compatibility
@@ -66,7 +66,7 @@ def prepare_material_entries(api, TestMat_Comp, TestMat_Ener):
     Prepare material entries and fetch entries from the Materials Project for a given material.
 
     Args:
-        api_key (str): API key for the Materials Project.
+        api: API key for the Materials Project.
         TestMat_Comp (str): Composition of the material.
         TestMat_Ener (float): Energy of the material.
 
@@ -241,54 +241,3 @@ def calculate_phase_diagram_condition_X(entriesTotal_X, entriesGases_X, locked_C
     # energy_above_hull_X = TestMat_entry_X.energy / TestMat_entry_X.composition.num_atoms - pd_X.get_hull_energy_per_atom(TestMat_entry_X.composition)
 
     return pd_X, energy_per_atom_X, formation_energy_X, energy_above_hull_X
-
-
-# # Main Function
-# def main():
-#     api = "kzum4sPsW7GCRwtOqgDIr3zhYrfpaguK"
-#     TestMat_Comp = "Ba8Zr8O24"
-#     TestMat_Ener = -333.71584216
-
-#     # Initialize global gas entries and chemical potentials
-#     initialize_global_variables()
-
-#     # Prepare material entries
-#     all_entries_A, all_entries_C, entriesTotal_X, TestMat_entry_A, TestMat_entry_C, TestMat_entry_X = prepare_material_entries(
-#         api, TestMat_Comp, TestMat_Ener
-#     )
-
-#     # Calculate phase diagrams and energies for each condition
-#     pd_A, energy_per_atom_A, formation_energy_A, energy_above_hull_A = calculate_phase_diagram_condition_A(
-#         all_entries_A, entriesGases_A, locked_Chem_Potential_A, TestMat_entry_A
-#     )
-#     pd_C, energy_per_atom_C, formation_energy_C, energy_above_hull_C = calculate_phase_diagram_condition_C(
-#         all_entries_C, entriesGases_C, locked_Chem_Potential_C, TestMat_entry_C
-#     )
-#     pd_X, energy_per_atom_X, formation_energy_X, energy_above_hull_X = calculate_phase_diagram_condition_X(
-#         entriesTotal_X, entriesGases_X, locked_Chem_Potential_X, TestMat_entry_X
-#     )
-
-#     # Print results for condition A (Hydrogen-rich)
-#     print("Condition A (Hydrogen-rich):")
-#     print("Phase diagram:", pd_A)
-#     print("Energy per atom:", energy_per_atom_A)
-#     print("Formation energy:", formation_energy_A)
-#     print("Energy above hull:", energy_above_hull_A)
-
-#     # Print results for condition C (Oxygen-rich)
-#     print("\nCondition C (Oxygen-rich):")
-#     print("Phase diagram:", pd_C)
-#     print("Energy per atom:", energy_per_atom_C)
-#     print("Formation energy:", formation_energy_C)
-#     print("Energy above hull:", energy_above_hull_C)
-
-#     # # Print results for condition X (CO2-rich)
-#     print("\nCondition X (CO2-rich):")
-#     print("Phase diagram:", pd_X)
-#     print("Energy per atom:", energy_per_atom_X)
-#     print("Formation energy:", formation_energy_X)
-#     print("Energy above hull:", energy_above_hull_X)
-
-
-# if __name__ == "__main__":
-#     main()
