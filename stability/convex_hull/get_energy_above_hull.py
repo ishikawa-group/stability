36da09b95c8e7498ec20a18b27e4e6a49c15f99b
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2025-01-04 13:22:55 +0900

    updated

--- stability/convex_hull/get_energy_above_hull.py
+++ stability/convex_hull/get_energy_above_hull.py
@@ -17,10 +17,10 @@ def get_energy_above_hull(atoms=None, energy=None):
 
     # Set test material and energy
     api = os.getenv("MAPI")
-    input_comp = atoms.get_chemical_formula()  # "Ba8Zr8O24"
+    input_comp = atoms.get_chemical_formula()
     input_energy = energy
 
-    print(f"input_comp: {input_comp}")
+    print(f"input_composition: {input_comp}")
     print(f"input_energy: {input_energy:8.6f}")
 
     # Initialize global variables
@@ -43,29 +43,26 @@ def get_energy_above_hull(atoms=None, energy=None):
     pd_C, e_per_atom_C, e_hull_C, e_hull_per_atom_C, e_above_hull_per_atom_C \
         = calculate_phase_diagram_condition(all_entries_C, entriesGases_C, chempot_C, input_C)
 
-    """
     pd_X, energy_per_atom_X, hull_energy_X, energy_above_hull_X = (
         calculate_phase_diagram_condition_X(
             entriesTotal_X, entriesGases_X, chempot_X, TestMat_entry_X))
-    """
+
     # --- Print results for condition A (Hydrogen-rich)
-    # print("Condition A (Hydrogen-rich):")
-    # print(f"Energy per atom: {e_per_atom_A:6.4f} eV/atom")
-    # print(f"Hull energy per_atom: {e_hull_per_atom_A:6.4f} eV/atom")
-    # print(f"Energy above hull per atom: {e_above_hull_per_atom_A:6.4f} eV/atom")
+    print("Condition A (Hydrogen-rich):")
+    print(f"Energy per atom: {e_per_atom_A:6.4f} eV/atom")
+    print(f"Hull energy per_atom: {e_hull_per_atom_A:6.4f} eV/atom")
+    print(f"Energy above hull per atom: {e_above_hull_per_atom_A:6.4f} eV/atom")
 
     # --- Print results for condition C (Oxygen-rich)
-    # print("\nCondition C (Oxygen-rich):")
-    # print(f"Energy per atom: {e_per_atom_C:6.4f} eV/atom")
-    # print(f"Hull energy per_atom: {e_hull_per_atom_C:6.4f} eV/atom")
-    # print(f"Energy above hull per atom: {e_above_hull_per_atom_C:6.4f} eV/atom")
+    print("\nCondition C (Oxygen-rich):")
+    print(f"Energy per atom: {e_per_atom_C:6.4f} eV/atom")
+    print(f"Hull energy per_atom: {e_hull_per_atom_C:6.4f} eV/atom")
+    print(f"Energy above hull per atom: {e_above_hull_per_atom_C:6.4f} eV/atom")
 
-    """
     # Print results for condition X (CO2-rich)
     print("\nCondition X (CO2-rich):")
     print(f"Energy per atom: {energy_per_atom_X:6.4f} eV/atom")
     print(f"Hull energy per_atom: {hull_energy_X:6.4f} eV/atom")
     print(f"Energy above hull per atom: {energy_above_hull_X:6.4f} eV/atom")
-    """
 
     return e_above_hull_per_atom_A, e_above_hull_per_atom_C
